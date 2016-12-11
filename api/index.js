f09309425cff891208d6a86d5f7d4c60b834a6c1
Troy Alford <troy@troyalford.com>
2016-12-10 16:50:53 -0800

    added download api

--- api/index.js
+++ api/index.js
@@ -1,43 +1,42 @@
 import express from 'express'
 import cheerio from 'cheerio'
 import fetch from 'isomorphic-fetch'
+import { uniqBy } from 'lodash'
 import moment from 'moment'
 import qs from 'querystring'
 
 export default express()
 .use('/search/:search_query', (request, response) => {
 	const query = request.params.search_query
+	const addons = []
 
-	const params = qs.stringify({
-		page: 1,
-		src_game: 1704,
-		src_name: query,
-		// src_descr: '', //'description contains...',
-		// src_auth: '',  //'author name contains...',
-		// src_upldr: '', //'uploader name contains...',
-		src_timeframe: 0,
+	const multisearch = Promise.all([
+		search({ src_name: query }, addons),
+		search({ src_descr: query }, addons),
+	])
+	.then(() => uniqBy(addons, 'id'))
+	.then(addons =>	response.status(200).send({ addons, query }))
+	.catch(console.error)
+})
+.use('/download/:id', (request, response) => {
+	const id = request.params.id
+	fetch(`http://www.nexusmods.com/skyrimspecialedition/ajax/downloadwithnmm?id=${id}`)
+	.then(response => response.text())
+	.then(html => cheerio.load(html))
+	.then(parser => parser('meta[http-equiv="refresh"]').first().attr('content'))
+	.then(meta => meta.replace(/^0;url='/, ''))
+	.then(url => response.redirect(url))
+})
 
-		ignoreCF: 0,
-
-		src_onlynmm: 1,
-		src_showadult: 1,
-
-		src_tab: 1,
-		src_view: 2,
-		src_language: 0,
-		src_order: 1,
-		src_sort: 0,
-		src_timeframe: 0,
-		src_cat: 0,
-	})
+function search(options = {}, addons = []) {
 	const search_url = 'http://www.nexusmods.com/skyrimspecialedition/mods/searchresults/'
+	const search_params = qs.stringify({ ...params, ...options })
 
-	fetch(`${search_url}?${params}`)
+	return fetch(`${search_url}?${search_params}`)
 	.then(response => response.text())
 	.then(html => cheerio.load(html))
 	.then(parser => parser('.files-list > li').toArray().map(el => {
 		const addon = parser(el)
-
 		return {
 			author: {
 				name: addon.find('a.user').first().text(),
@@ -71,11 +70,28 @@ export default express()
 			updated: toDate(addon.updated.replace(/updated /, '')),
 		}
 	}))
-	.then(addons => {
-		response.status(200).send({
-			addons,
-			query,
-		})
-	})
-	.catch(console.error)
-})
+	.then(final => final.map(addon => addons.push(addon)))
+}
+
+const params = {
+	page: 1,
+	src_game: 1704,
+	// src_name: '',
+	// src_descr: '',
+	// src_auth: '',
+	// src_upldr: '',
+	src_timeframe: 0,
+
+	ignoreCF: 0,
+
+	src_onlynmm: 1,
+	src_showadult: 1,
+
+	src_tab: 1,
+	src_view: 2,
+	src_language: 0,
+	src_order: 1,
+	src_sort: 0,
+	src_timeframe: 0,
+	src_cat: 0,
+}
